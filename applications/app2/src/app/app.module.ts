a7405f3d6fec3aa1a3afaf8dfe2bd5b7358e82db
Walter Leinert <walter.leinert@aracom.de>
2018-07-18 12:01:35 +0200

    moved app2 to applications

--- applications/app2/src/app/app.module.ts
+++ applications/app2/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ComponentsModule } from 'components';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
